20cecbd2b5c262e261a73dfd55f98ffcf918785a
saaruniversity <saaruniversity@gmail.com>
2016-02-28 14:26:13 +0200

    database connection

--- index.php
+++ index.php
@@ -2,19 +2,10 @@
 
 <div><p>Something is here!</p> </div>
 
-
 <?php
-$query = "SELECT first_name, last_name "
-     . "FROM employees";
-$result = $db->query($query);
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    echo "<tr>";
-    echo "<td>" . htmlspecialchars($row["last_name"]) . "</td>";
-    echo "<td>" . htmlspecialchars($row["first_name"]) . "</td>";
-    echo "</tr>";
-}
-$result->closeCursor();
+$db = pg_connect("host=ec2-107-21-229-87.compute-1.amazonaws.com port=5432 dbname=d4f6m4r9oo8790 user=qdlwaqfognpves password=9bVaZL0EyHA9GAI0sV_dJs4wTW");  
+$result = pg_query($db, "SELECT first_name, last_name FROM employees limit 1");  
+$row = pg_fetch_assoc($result);  
+echo "<p>Done! $row[first_name]</p>"
 ?>
-
-
 <?php include ("footer.php"); ?>
