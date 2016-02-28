4d0ab6c51d205111fe2051f5ad2d951c33ad1e40
saaruniversity <saaruniversity@gmail.com>
2016-02-28 14:19:30 +0200

    databse connection

--- index.php
+++ index.php
@@ -2,4 +2,19 @@
 
 <div><p>Something is here!</p> </div>
 
+
+<?php
+$query = "SELECT first_name, last_name "
+     . "FROM employees";
+$result = $db->query($query);
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    echo "<tr>";
+    echo "<td>" . htmlspecialchars($row["last_name"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["first_name"]) . "</td>";
+    echo "</tr>";
+}
+$result->closeCursor();
+?>
+
+
 <?php include ("footer.php"); ?>
